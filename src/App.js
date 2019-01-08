2185cacbd3c45039aece8578262c67d0932ca575
ChauDinh <chaudvb95uc@gmail.com>
2019-01-08 19:16:10 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -23,7 +23,12 @@ class App extends Component {
     ]
   }
   markComplete = (id) => {
-    console.log(id);
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
   }
   render() {
     return (
